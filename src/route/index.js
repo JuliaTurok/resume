8fa32134146403628e6c6f0f87ce1b8e975d36d8
JuliaTurok <107355921+JuliaTurok@users.noreply.github.com>
2023-05-15 12:09:55 +0300

    added layout

--- src/route/index.js
+++ src/route/index.js
@@ -206,7 +206,7 @@ router.get('/work', function (req, res) {
 router.get('/person', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('person', {
-    layout: 'person',
+    layout: 'basic',
     person: {
       name: 'Emma Johnson',
       age: 32,
@@ -306,7 +306,7 @@ router.get('/person', function (req, res) {
 router.get('/bio', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('bio', {
-    layout: 'bio',
+    layout: 'basic',
     name: 'Albert Einstein',
     birthdate: 'March 14, 1879',
     birthplace:
